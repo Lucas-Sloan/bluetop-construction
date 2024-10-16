d32c97e919346a03a8298294f727f4e2680209b6
Lucas-Sloan <Lucasg.sloan@gmail.com>
2024-10-16 15:23:14 -0500

    Added HTML and CSS for footer, adjusted Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Landing from './components/Landing';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Landing />
+      <Footer />
     </div>
   );
-};  
+};
 
 export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from '../assets/Logo.png';
+import facebookIcon from '../assets/Facebook.png';
+import instagramIcon from '../assets/Instagram.png';
+import linkedinIcon from '../assets/Linkedin.png';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="contact-info">
+        <p>Contact us:</p>
+        <p>Phone: (817) 366-8739</p>
+        <p>Email: Arrmerchen@thebluetop.com</p>
+        <div className="rights">
+            <p>© 2023 Bluetop Construction. All Rights Reserved.</p>
+        </div>
+      </div>
+      <img src={logo} alt="Logo" className="logo" />
+      <div className="social-icons">
+        <img src={facebookIcon} alt="Facebook" className="icon" />
+        <img src={instagramIcon} alt="Instagram" className="icon" />
+        <img src={linkedinIcon} alt="LinkedIn" className="icon" />
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
