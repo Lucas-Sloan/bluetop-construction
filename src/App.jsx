91fdc1a72e96ac92d0e486d4f8cc8ef276144910
Lucas-Sloan <Lucasg.sloan@gmail.com>
2024-10-22 16:43:23 -0500

    Adjusted app styling and overall styling

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,24 @@ import Quote from './components/Quote';
 import Footer from './components/Footer';
 import Careers from './components/Careers';
 import Services from './components/Services';
+import './App.css'; // Ensure you have this imported for the .app-container styles
 
 const App = () => {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/quote" element={<Quote />} />
-        <Route path="/careers" element={<Careers />} />
-        <Route path="/services" element={<Services />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <div className="app-container"> {/* Add the app-container here */}
+      <Router>
+        <Navbar />
+        <div className="main-content"> {/* This div allows the content to grow and push the footer down */}
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/quote" element={<Quote />} />
+            <Route path="/careers" element={<Careers />} />
+            <Route path="/services" element={<Services />} />
+          </Routes>
+        </div>
+        <Footer />
+      </Router>
+    </div>
   );
 };
 
